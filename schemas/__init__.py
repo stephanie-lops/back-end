9433307b2ccd8333fb5fb42122450230433ac9c6
stephanie-lops <stephanielopees@gmail.com>
2024-07-07 18:39:01 -0400

    Delete schemas directory

--- schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from schemas.comentario import ComentarioSchema
-from schemas.produto import ProdutoSchema, ProdutoBuscaSchema, ProdutoViewSchema, \
-                            ListagemProdutosSchema, ProdutoDelSchema, apresenta_produtos, \
-                            apresenta_produto, apresenta_produtos
-from schemas.error import ErrorSchema
